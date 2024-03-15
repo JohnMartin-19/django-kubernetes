74066086ff1723d7311651f2e805ac2f1e36a1f5
John <johnnkonge2020@gmail.com>
2024-03-15 07:01:22 +0300

    created docker compose image and container

---
+++ src/migrate.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+SUPER_USER_EMAIL = {DJANGO_SUPERUSER_EMAIL}
+cd /app/
+
+#/opt/.venv/bin/python3 manage.py makemigrations --noinput
+/opt/.venv/bin/python3 manage.py migrate --noinput
+/opt/.venv/bin/python3 manage.py createsuperuser --email $SUPER_USER_EMAIL --noinput || true
